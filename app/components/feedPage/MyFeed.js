cb0d20e047836cf621f5613abe8797ee634afec5
spencer-charney <root@ip-172-31-18-35.us-east-2.compute.internal>
2020-03-08 18:53:48 +0000

    Turned Recipe into an object that accepts props

--- app/components/feedPage/MyFeed.js
+++ app/components/feedPage/MyFeed.js
@@ -11,7 +11,12 @@ class MyFeed extends React.Component {
     }
     render() {
       //const postList = this.props.postList;
-      const posts = [<Review />,<Recipe />,<Review />];
+      const posts = [<Review />,<Recipe title={"WeedKrispies"} author={"Recipe by Briiiian"} description={"These tasty vegan snacks are sure to have you shmeezing"} 
+      totalTime={"4-5 hours"} serves={16} tags={["Vegan", "Gluten Free", "Edible"]} ingredients={["3.5 grams of gas","3/4ths of a cup of coconut oil", "5 cups of vegan marshmallows", "6 cups of puffed rice cereal"]}
+       directions={["Preheat oven to 240F","Grind weed and put on a pan or foil bowl","Put weed in the oven for 45 minutes to decarb","In a small pot, heat coconut oil to 185F-215F",
+       "When the weed is ready to come out of the oven, add it to the oil and let cook for 2 hours","After the 2 hours, add the marshmallows to a large pan and stir over low heat until melted",
+       "Strain the oil in order to remove all the flower, and add the oil to the marshmallows","Stir in the puffed rice cereal and continue to stir until evenly mixed",
+       "Transfer this mixture into a 13x9 pan and put in the refridgerator for 1-2 hours","Cut and enjoy"]}/>,<Review />];
       const postList = posts.map((post)=> 
         <li key={post.props.id}>
           <Post>{post}</Post>
--- app/components/feedPage/Recipe.js
+++ app/components/feedPage/Recipe.js
@@ -28,52 +28,46 @@ class Recipe extends React.Component {
     }
   }
   render() {
-    let ingredients;
+    const ingredientsList = this.props.ingredients.map((ingredient) => 
+      <li>{ingredient}</li>
+    );
+    let ingredientsUl;
     if (this.state.ingredientsClicked) {
-      ingredients = <ul id="ingredients">
-        <li>3.5 grams of gas</li>
-        <li>3/4ths of a cup of coconut oil</li>
-        <li>5 cups of vegan marshmallows</li>
-        <li>6 cups of puffed rice cereal</li>
+      ingredientsUl = <ul id="ingredients">
+        {ingredientsList}
       </ul>
     }
     else {
-      ingredients = <div></div>
+      ingredientsUl = <div></div>
     }
-    let directions;
+    const directionsList = this.props.directions.map((direction) => 
+      <li>{direction}</li>
+    );    
+    let directionsOl;
     if (this.state.directionsClicked) {
-      directions = <ol id="directions">
-        <li>Preheat oven to 240&deg;F</li>
-        <li>Grind weed and put on a pan or foil bowl</li>
-        <li>Put weed in the oven for 45 minutes to decarb</li>
-        <li>In a small pot, heat coconut oil to 185&deg;F-215&deg;F</li>
-        <li>When the weed is ready to come out of the oven, add it to the oil and let cook for 2 hours</li>
-        <li>After the 2 hours, add the marshmallows to a large pan and stir over low heat until melted</li>
-        <li>Strain the oil in order to remove all the flower, and add the oil to the marshmallows</li>
-        <li>Stir in the puffed rice cereal and continue to stir until evenly mixed</li>
-        <li>Transfer this mixture into a 13x9 pan and put in the refridgerator for 1-2 hours</li>
-        <li>Cut and enjoy</li>
+      directionsOl = <ol id="directions">
+        {directionsList}
       </ol>
     }
     else {
-      directions = <div></div>
+      directionsOl = <div></div>
     }
     return (
       <div className="recipe">
         <div className="container">
           <center>
-            <h1>WeedKrispies</h1>
-            <p id="other-user"><i>Recipe by Briiiian</i></p>
-            <p>These tasty vegan snacks are sure to have you shmeezing</p>
+            <h1>{this.props.title}</h1>
+            <p id="other-user"><i>{this.props.author}</i></p>
+    <p>{this.props.description}</p>
 
             <div className="row justify-content-md-center details">
               <div className="col-sm-auto">
                 <p>Total Time</p>
-                <p>4-5 hours</p>
+    <p>{this.props.totalTime}</p>
               </div>
               <div className="col-sm-auto">
                 <p>Serves</p>
-                <p>16</p>
+    <p>{this.props.serves}</p>
               </div>
             </div>
             <div className="row justify-content-md-center">
@@ -91,11 +85,11 @@ class Recipe extends React.Component {
         </div>
         <div className="container">
           <h2 onClick={this.handleClickIngredients}>Ingredients</h2>
-          {ingredients}
+          {ingredientsUl}
         </div>
         <div className="container">
           <h2 onClick={this.handleClickDirections}>Directions</h2>
-          {directions}
+          {directionsOl}
         </div>
       </div>
     )
