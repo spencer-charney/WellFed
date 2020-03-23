0107918eb4d3772a21de70381cd54009f3e37bc3
spencer-charney <root@ip-172-31-18-35.us-east-2.compute.internal>
2020-03-23 18:22:38 +0000

    Added some authorization things

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const {getPosts, createPost} = require('../controllers/post');
 const {requireSignin} = require('../controllers/auth');
+const {userById} = require('../controllers/user');
 const validator = require('../validator');
 
 const router = express.Router();
 
-router.get('/', requireSignin, getPosts);
-router.post('/post', validator.createPostValidator, createPost);
+router.get('/', getPosts);
+router.post('/post', requireSignin, validator.createPostValidator, createPost);
+
+// any route containing :userId, our app will first execute userById()
+router.param("userId", userById);
 
 module.exports = router;
