bff2073ca3be53bc96abd85b1e3683470b510b19
spencer-charney <root@ip-172-31-18-35.us-east-2.compute.internal>
2020-03-23 19:14:38 +0000

    Updating user possible

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -20,3 +20,37 @@ exports.hasAuthorization = (req, res, next) => {
 		})
 	}
 }
+
+exports.allUsers = (req, res) => {
+	User.find((err, users) => {
+		if (err) {
+			return res.status(400).json({
+				error: err
+			});
+		}
+
+		res.json({users});
+	}).select("name email updated created")
+};
+
+exports.getUser = (req, res) => {
+	req.profile.hashed_password = undefined;
+	req.profile.salt = undefined;
+	return res.json(req.profile);
+}
+
+exports.updateUser = (req, res, next) => {
+	let user = req.profile;
+	user = _.extend(user, req.body);
+	user.updated = Date.now()
+	user.save((err) => {
+		if (err) {
+			return res.status(400).json({
+				error: "You are not authorized to perform this action."
+			})
+		}
+		user.hashed_password = undefined;
+		user.salt = undefined;
+		res.json({ user })
+	})
+};
