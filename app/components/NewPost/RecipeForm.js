1448bca558ebad2fa662b3f0163977c991ab55d7
spencer-charney <root@ip-172-31-18-35.us-east-2.compute.internal>
2020-03-26 19:04:39 +0000

    Turned recipeForm into a react object

--- app/components/NewPost/RecipeForm.js
+++ app/components/NewPost/RecipeForm.js
@@ -1,21 +1,124 @@
 import React from 'react';
 
 class RecipeForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            description: '',
+            totalTime: '',
+            serves: '',
+            tags: '',
+            ingredients: '',
+            directions: ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChangeTitle = this.handleChangeTitle.bind(this);
+        this.handleChangeDescription = this.handleChangeDescription.bind(this);
+        this.handleChangeTotalTime = this.handleChangeTotalTime.bind(this);
+        this.handleChangeServes = this.handleChangeServes.bind(this);
+        this.handleChangeTags = this.handleChangeTags.bind(this);
+        this.handleChangeIngredients = this.handleChangeIngredients.bind(this);
+        this.handleChangeDirections = this.handleChangeDirections.bind(this);
+    }
+
+    handleSubmit(event) {
+        //THis is where one would send a message to the server
+        console.log(this.state.title);
+        console.log(this.state.description);
+        console.log(this.state.totalTime);
+        console.log(this.state.serves);
+        console.log(this.state.tags);
+        console.log(this.state.ingredients);
+        console.log(this.state.directions);
+        //See if one can remove the below line and still post to the server
+        event.preventDefault();
+    }
+
+    handleChangeTitle(event) {
+        this.setState({ title: event.target.value });
+    }
+
+    handleChangeDescription(event) {
+        this.setState({ description: event.target.value });
+    }
+
+    handleChangeTotalTime(event) {
+        this.setState({ totalTime: event.target.value });
+
+    }
+
+    handleChangeServes(event) {
+        this.setState({ serves: event.target.value });
+
+    }
+
+    handleChangeTags(event){
+        this.setState({ tags: event.target.value});
+    }
+
+    handleChangeIngredients(event) {
+        this.setState({ ingredients: event.target.value });
+
+    }
+
+    handleChangeDirections(event) {
+        this.setState({ directions: event.target.value });
+
+    }
+
     render() {
         return (
             <div>
                 <center>
-                <h2>New Recipe</h2>
+                    <h2>New Recipe</h2>
                 </center>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div className="form-group">
-                        <label className="form-label">Title: </label>
-                        <input type="text" className="form-control" id="title" placeholder="Title"></input>
+                        <label>
+                            Title:
+                        </label>
+                        <input type="text" value={this.state.title} onChange={this.handleChangeTitle} className="form-control" placeholder="Title of Recipe"/>
                     </div>
                     <div className="form-group">
-                        <label className="form-label">Description: </label>
-                        <textarea type="text-area" className="form-control" id="description" placeholder="Please describe this dish" rows="3"></textarea>
+                        <label >
+                            Description:
+                        </label>
+                        <textarea value={this.state.description} onChange={this.handleChangeDescription} className="form-control" placeholder="Please describe your dish"/>
                     </div>
+                    <div className="form-group">
+                        <label >
+                            Total Time:
+                        </label>
+                        <input type="text" value={this.state.totalTime} onChange={this.handleChangeTotalTime} className="form-control" placeholder="Please specify unit of time (minutes or hours)"/>
+                    </div>
+                    <div className="form-group">
+                        <label >
+                            Serves:
+                        </label>
+                        <input type="text" value={this.state.serves} onChange={this.handleChangeServes} className="form-control" placeholder="Servings"/>
+                    </div>
+                    <div className="form-group">
+                        <label>
+                            Tags:
+                        </label>
+                        <input type="text" value={this.state.tags} onChange={this.handleChangeTags} className="form-control" placeholder="Please separate values with a comma (eg. Vegan, Gluten Free, etc.)" />
+                    </div>
+                    <div className="form-group">
+                        <label >
+                            Ingredients:
+                        </label>
+                        <textarea value={this.state.ingredients} onChange={this.handleChangeIngredients} rows="6" className="form-control" placeholder="Please list each ingredient on a separate line"/>
+                    </div>
+                    <div className="form-group">
+                        <label>
+                            Directions:
+                        </label>
+                        <textarea value={this.state.directions} onChange={this.handleChangeDirections} rows="6" className="form-control" placeholder="Please number each direction and place it on a separate line"/>
+                    </div>
+                    <center>                    
+                        <input type="submit" value="Post" className="btn btn-primary"/>
+                    </center>
                 </form>
             </div>
         )
