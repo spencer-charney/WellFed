88d1f57150ccb68fbec1ec4f37541738fd2a1436
spencer-charney <root@ip-172-31-18-35.us-east-2.compute.internal>
2020-03-30 00:24:31 +0000

    updated gitignore

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -5,7 +5,8 @@ const {
 	postsByUser, 
 	postById,
 	isPoster,
-	deletePost
+	deletePost,
+	updatePost
 } = require('../controllers/post');
 const {requireSignin} = require('../controllers/auth');
 const {userById} = require('../controllers/user');
@@ -13,7 +14,7 @@ const {createPostValidator} = require('../validator');
 
 const router = express.Router();
 
-router.get('/', getPosts);
+router.get('/posts', getPosts);
 router.post(
 	'/post/new/:userId', 
 	requireSignin, 
@@ -21,6 +22,7 @@ router.post(
 	createPostValidator
 );
 router.get('/posts/by/:userId', requireSignin, postsByUser);
+router.put('/post/:postId', requireSignin, isPoster, updatePost);
 router.delete('/post/:postId', requireSignin, isPoster, deletePost);
 
 // any route containing :userId, our app will first execute userById()
