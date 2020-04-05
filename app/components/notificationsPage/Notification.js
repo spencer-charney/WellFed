4acbad786fc87fd8b31d567a5501b5924628d4ff
spencer-charney <root@ip-172-31-18-35.us-east-2.compute.internal>
2020-04-05 16:32:32 +0000

    Updated NotificationsPage and Notification

--- app/components/notificationsPage/Notification.js
+++ app/components/notificationsPage/Notification.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import '../../css/Notification.css'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 class Notification extends React.Component {
-    constructor(props){
-        super(props);
-        
-    }
-    render() {
+   constructor(props) {
+      super(props);
+
+   }
+   render() {
+      let notification;
+      switch (this.props.type) {
+         case "newComment":
+            notification = <Container fluid>
+               <Row>New Comment from {this.props.username}</Row>
+               <Row><i>{this.props.time}</i></Row>
+               <Row>{this.props.message}</Row>
+            </Container>
+            break;
+         case "newBookmark":
+            notification = <Container fluid>
+               <Row>New Bookmark from {this.props.username}</Row>
+               <Row><i>{this.props.time}</i></Row>
+               <Row>{this.props.message}</Row>
+            </Container>
+            break;
+         case "newFollow":
+            notification = <Container fluid>
+               <Row>New Follow from {this.props.username}</Row>
+               <Row><i>{this.props.time}</i></Row>
+            </Container>
+            break;
+      }
       return (
-         <div>
-            <hr />
-            <div className="notification">{this.props.children}</div>
-         </div>   
+         <Row>{notification}</Row>
       )
-    }
-  }
-  
-  export default Notification;
+   }
+}
+
+export default Notification;
--- app/components/notificationsPage/NotificationsPage.js
+++ app/components/notificationsPage/NotificationsPage.js
@@ -1,39 +1,25 @@
 import React from 'react';
 import '../../css/Notification.css'
-import NewFollowerNotification from './NewFollowerNotification';
 import Notification from './Notification'
-import NewLikeNotification from './NewLikeNotification';
-import NewCommentNotification from './NewCommentNotification';
-import NewDogEarNotification from './NewDogEarNotificaiton';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 class NotificationsPage extends React.Component {
     constructor(props){
       super(props);
     }
     render() {
-      //const notificationsList = this.props.notificationsList;
-      const notifications = [<NewFollowerNotification otherUser="Brian Greeley" time="12:00" id='1'/>,
-      <NewFollowerNotification otherUser="Emily Schneider" time="12:01" id='2'/>,
-      <NewFollowerNotification otherUser="Mardi Schneider" time="12:02" id='3'/>,
-      <NewLikeNotification otherUser="Emily Schneider" post="post1" time="12:03" id='4' />,
-      <NewLikeNotification otherUser="Mardi Schneider" post="post1" time="12:03" id='5' />,
-      <NewLikeNotification otherUser="Jen Schneider" post="post1" time="12:03" id='6' />,
-      <NewLikeNotification otherUser="Will Schneider" post="post1" time="12:03" id='7' />,
-      <NewLikeNotification otherUser="Julia Schneider" post="post1" time="12:03" id='8' />];
-      const notificationsList = notifications.map((newNotification)=> 
-        <li key={newNotification.props.id}>
-          <Notification>{newNotification}</Notification>
-        </li>     
-      );
+      let notificationsArray = [];
+      var len = this.props.notifications.length;
+      for(var i = 0; i < len; i++){
+        notificationsArray.push(
+          <Notification key={i} type={this.props.notifications[i].type} username={this.props.notifications[i].username} time={this.props.notifications[i].time} message={this.props.notifications[i].message}/>
+        );
+      }
       return (
-        <center>
-          <h1>Notifications</h1>
-          <div className="scroll">
-            <ul>
-              {notificationsList}
-            </ul>
-          </div>
-        </center>
+        <Container fluid>
+          {notificationsArray}
+        </Container>
       )
     }
   }
