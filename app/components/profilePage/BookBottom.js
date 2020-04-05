93c2861589e7a2a2a6d943b3ae1efaa69ce1fe4e
spencer-charney <root@ip-172-31-18-35.us-east-2.compute.internal>
2020-04-05 22:51:24 +0000

    Made posts come from index for MyProfile

--- app/components/profilePage/BookBottom.js
+++ app/components/profilePage/BookBottom.js
@@ -2,8 +2,9 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { Button } from 'react-bootstrap';
+import Recipe from '../feedPage/Recipe'
+import Review from '../feedPage/Review'
+
 
 class BookBottom extends React.Component {
     constructor(props) {
@@ -15,8 +16,17 @@ class BookBottom extends React.Component {
         var len = this.props.posts.length;
         let arrayRows = [];
         for (var i = 0; i < len; i++) {
+            let post;
+            if (this.props.posts[i].type == 'recipe') {
+                post = <Recipe title={this.props.posts[i].title} author={this.props.posts[i].author} description={this.props.posts[i].description} totalTime={this.props.posts[i].totalTime} serves={this.props.posts[i].serves} tags={this.props.posts[i].tags} ingredients={this.props.posts[i].ingredients} directions={this.props.posts[i].directions} comments={this.props.posts[i].comments} />;
+            }
+            else {
+                //review
+                post = <Review restaurant={this.props.posts[i].restaurant} dish={this.props.posts[i].dish} user={this.props.posts[i].user} rate={this.props.posts[i].rate} tags={this.props.posts[i].tags} review={this.props.posts[i].review} comments={this.props.posts[i].comments} />;
+
+            }
             arrayRows.push(
-                <Row key={i}>{this.props.posts[i]}</Row>
+                <Row key={i}>{post}</Row>
             )
         }
         return (
--- app/components/profilePage/MyPosts.js
+++ app/components/profilePage/MyPosts.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { Button } from 'react-bootstrap';
+import Recipe from '../feedPage/Recipe'
+import Review from '../feedPage/Review'
 
 class MyPosts extends React.Component {
     constructor(props) {
@@ -14,8 +14,17 @@ class MyPosts extends React.Component {
         var len = this.props.myPosts.length;
         let arrayRows = [];
         for (var i = 0; i < len; i++) {
+            let post;
+            if (this.props.myPosts[i].type == 'recipe') {
+                post = <Recipe title={this.props.myPosts[i].title} author={this.props.myPosts[i].author} description={this.props.myPosts[i].description} totalTime={this.props.myPosts[i].totalTime} serves={this.props.myPosts[i].serves} tags={this.props.myPosts[i].tags} ingredients={this.props.myPosts[i].ingredients} directions={this.props.myPosts[i].directions} comments={this.props.myPosts[i].comments} />;
+            }
+            else {
+                //review
+                post = <Review restaurant={this.props.myPosts[i].restaurant} dish={this.props.myPosts[i].dish} user={this.props.myPosts[i].user} rate={this.props.myPosts[i].rate} tags={this.props.myPosts[i].tags} review={this.props.myPosts[i].review} comments={this.props.myPosts[i].comments} />;
+
+            }
             arrayRows.push(
-                <Row key={i}>{this.props.myPosts[i]}</Row>
+                <Row key={i}>{post}</Row>
             )
         }
         return (
