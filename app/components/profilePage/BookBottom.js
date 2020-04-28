0984f79adf4fbf4781da9dac5da72190db45ec41
spencer-charney <root@ip-172-31-18-35.us-east-2.compute.internal>
2020-04-28 21:40:14 +0000

    A lot of stuff done

--- app/components/profilePage/BookBottom.js
+++ app/components/profilePage/BookBottom.js
@@ -5,12 +5,31 @@ import Row from 'react-bootstrap/Row';
 import Recipe from '../feedPage/Recipe'
 import Review from '../feedPage/Review'
 import Col from 'react-bootstrap/Col';
-
+import {getUser, isAuthenticated} from '../landingPage/Auth'
 
 
 class BookBottom extends React.Component {
     constructor(props) {
         super(props);
+        this.getUsername = this.getUsername.bind(this);
+        this.state = {
+            user: ''
+        }
+    }
+
+    getUsername(userId) {
+        getUser(userId, isAuthenticated().token).then(data => {
+            if (data.error) {
+                this.setState({
+                    error: data.error
+                })
+            } 
+            else {
+                this.setState({ 
+                    user: data
+                });
+            }
+        });
     }
 
 
@@ -30,13 +49,20 @@ class BookBottom extends React.Component {
         else {
             var len = this.props.posts.length;
             for (var i = 0; i < len; i++) {
+                this.getUsername(this.props.posts[i].postedBy);
+
                 let post;
                 if (this.props.posts[i].type == 'recipe') {
-                    post = <Recipe title={this.props.posts[i].title} author={this.props.posts[i].author} description={this.props.posts[i].description} totalTime={this.props.posts[i].totalTime} serves={this.props.posts[i].serves} tags={this.props.posts[i].tags} ingredients={this.props.posts[i].ingredients} directions={this.props.posts[i].directions} comments={this.props.posts[i].comments} />;
+                    post = <Recipe self={this.props.self} clickUser={this.props.clickUser} postId={this.props.posts[i]._id} title={this.props.posts[i].title} username={this.state.user.username} author={this.state.user.username} description={this.props.posts[i].description} totalTime={this.props.posts[i].totalTime} serves={this.props.posts[i].serves} tags={this.props.posts[i].tags} ingredients={this.props.posts[i].ingredients} directions={this.props.posts[i].directions} comments={this.props.posts[i].comments} />;
+
                 }
                 else {
+
+
+                    post = <Review self={this.props.self} clickUser={this.props.clickUser} postId={this.props.posts[i]._id} restaurant={this.props.posts[i].restaurant} username={this.state.user.username} dish={this.props.posts[i].dish} user={this.state.user.username} rate={this.props.posts[i].rate} tags={this.props.posts[i].tags} review={this.props.posts[i].review} comments={this.props.posts[i].comments} />;
+
                     //review
-                    post = <Review restaurant={this.props.posts[i].restaurant} dish={this.props.posts[i].dish} user={this.props.posts[i].user} rate={this.props.posts[i].rate} tags={this.props.posts[i].tags} review={this.props.posts[i].review} comments={this.props.posts[i].comments} />;
+                    // post = <Review restaurant={this.props.posts[i].restaurant} dish={this.props.posts[i].dish} user={this.props.posts[i].user} rate={this.props.posts[i].rate} tags={this.props.posts[i].tags} review={this.props.posts[i].review} comments={this.props.posts[i].comments} />;
     
                 }
                 arrayRows.push(
